9a99a8f93d5e7d249942e7ac1e41a9e9ba90ae0f
surfer92 <rosen.petrov92@gmail.com>
2015-01-14 17:26:33 +0200

    HomeWork
    dada

---
+++ AfterTenYears/AfterTenYears.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class AfterTenYears
+{
+    static void Main()
+    {
+        Console.WriteLine("На колко си години");
+        byte age = byte.Parse(Console.ReadLine());
+        Console.WriteLine("След 10 години ще си на "+(age+10));
+    }
+}
+
---
+++ NumberSequence/NumberSequence.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class NumberSequence
+{
+    static void Main()
+    {
+        int numberToPrint;
+        for (int i = 2; i <= 11; i++)
+        {
+            if (i%2==0)
+            {
+                numberToPrint=i;
+            }
+            else
+            {
+                numberToPrint = i * (-1);
+            }
+            Console.WriteLine(numberToPrint);
+        }
+    }
+}
+
